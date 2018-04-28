402c63521699eb8382062d636723bebf684d5f00
zhengxiangyang <zxy@xinshi.net>
2018-04-28 16:20:32 +0800

    提交CLION的工程

---
+++ utils/cmake-build-debug/CMakeFiles/utils.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utils.dir/main.c.o"
+  "CMakeFiles/utils.dir/Users/zhengxiangyang/work/fjt/fjtsrc/convert_utils.c.o"
+  "utils.pdb"
+  "utils"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/utils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
