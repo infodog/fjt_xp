b00b055ec7197e14934eadd209f398450ba80f36
zhengxiangyang <zxy@xinshi.net>
2019-05-18 16:45:47 +0800

    use $dist in fjt Makefile and modules.mk

--- modules.mk
+++ modules.mk
@@ -2,5 +2,5 @@
 	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_fjt.lo config_utils.lo license.lo convert.lo exportapi.lo baseutil.lo stack.lo ifconfigip.lo memstream.lo convert_utils.lo htmlparser.lo notfoundurls.lo
 DISTCLEAN_TARGETS = modules.mk
 shared =  mod_fjt.la
-export MOD_CFLAGS = -std=c99  -I$dist/fjtv3ssl/openssl-1.1.1b/include -Wno-error=declaration-after-statement
-export MOD_LDFLAGS = -L$dist/openssl-1.1.1b/lib -lssl -lcrypto -lpthread
+export MOD_CFLAGS = -std=c99  -I$(dist)/fjtv3ssl/openssl-1.1.1b/include -Wno-error=declaration-after-statement
+export MOD_LDFLAGS = -L$(dist)/openssl-1.1.1b/lib -lssl -lcrypto -lpthread
