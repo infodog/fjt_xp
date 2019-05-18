68f2cc2efb8fbebf439f6a4d522675a865796dd4
zhengxiangyang <zxy@xinshi.net>
2019-05-18 13:59:32 +0800

    release2019-05-18

---
+++ buildScripts/fjtbuild/buildtemp/httpd-2.4.38/include/ap_config_layout.h
@@ -0,0 +1,64 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file  ap_config_layout.h
+ * @brief Apache Config Layout
+ */
+
+#ifndef AP_CONFIG_LAYOUT_H
+#define AP_CONFIG_LAYOUT_H
+
+/* Configured Apache directory layout */
+#define DEFAULT_PREFIX "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl"
+#define DEFAULT_EXP_EXEC_PREFIX "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl"
+#define DEFAULT_REL_EXEC_PREFIX ""
+#define DEFAULT_EXP_BINDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/bin"
+#define DEFAULT_REL_BINDIR "bin"
+#define DEFAULT_EXP_SBINDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/bin"
+#define DEFAULT_REL_SBINDIR "bin"
+#define DEFAULT_EXP_LIBEXECDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/modules"
+#define DEFAULT_REL_LIBEXECDIR "modules"
+#define DEFAULT_EXP_MANDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/man"
+#define DEFAULT_REL_MANDIR "man"
+#define DEFAULT_EXP_SYSCONFDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/conf"
+#define DEFAULT_REL_SYSCONFDIR "conf"
+#define DEFAULT_EXP_DATADIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl"
+#define DEFAULT_REL_DATADIR ""
+#define DEFAULT_EXP_INSTALLBUILDDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/build"
+#define DEFAULT_REL_INSTALLBUILDDIR "build"
+#define DEFAULT_EXP_ERRORDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/error"
+#define DEFAULT_REL_ERRORDIR "error"
+#define DEFAULT_EXP_ICONSDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/icons"
+#define DEFAULT_REL_ICONSDIR "icons"
+#define DEFAULT_EXP_HTDOCSDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/htdocs"
+#define DEFAULT_REL_HTDOCSDIR "htdocs"
+#define DEFAULT_EXP_MANUALDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/manual"
+#define DEFAULT_REL_MANUALDIR "manual"
+#define DEFAULT_EXP_CGIDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/cgi-bin"
+#define DEFAULT_REL_CGIDIR "cgi-bin"
+#define DEFAULT_EXP_INCLUDEDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/include"
+#define DEFAULT_REL_INCLUDEDIR "include"
+#define DEFAULT_EXP_LOCALSTATEDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl"
+#define DEFAULT_REL_LOCALSTATEDIR ""
+#define DEFAULT_EXP_RUNTIMEDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/logs"
+#define DEFAULT_REL_RUNTIMEDIR "logs"
+#define DEFAULT_EXP_LOGFILEDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/logs"
+#define DEFAULT_REL_LOGFILEDIR "logs"
+#define DEFAULT_EXP_PROXYCACHEDIR "/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/proxy"
+#define DEFAULT_REL_PROXYCACHEDIR "proxy"
+
+#endif /* AP_CONFIG_LAYOUT_H */
---
+++ buildScripts/fjtbuild/buildtemp/httpd-2.4.38/modules/http2/modules.mk
@@ -0,0 +1,6 @@
+DISTCLEAN_TARGETS = modules.mk
+static = 
+shared = 
+MOD_CFLAGS = -I/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/openssl-1.1.1b/include
+MOD_CPPFLAGS = -DH2_OPENSSL
+MOD_LDFLAGS = -L/Users/zhengxiangyang/work/fjt/fjt_xp/buildScripts/fjtbuild/buildtemp/fjtv3ssl/openssl-1.1.1b/lib -lssl -lcrypto -lpthread
